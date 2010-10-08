2de3270f17292da643712a1c630663f1ae60e6e7
Garry Hill <garry@magnetised.info>
2010-10-08 16:21:24 +0100

    tidied page test

--- test/unit/test_page.rb
+++ test/unit/test_page.rb
@@ -49,11 +49,14 @@ class PageTest < Test::Unit::TestCase
       @p = Page.create
       @q = Page.new
       @r = Page.new
+      @s = Page.new
       @p << @q
       @q << @r
+      @q << @s
       @p.save
       @q.save
       @r.save
+      @s.save
     end
 
     should "have the right entry classes" do
@@ -65,22 +68,26 @@ class PageTest < Test::Unit::TestCase
       @p.parent.should be_nil
       @q.parent.should === @p
       @r.parent.should === @q
+      @s.parent.should === @q
     end
     should "have a list of their children" do
       @p.children.should == [@q]
-      @q.children.should == [@r]
+      @q.children.should == [@r, @s]
       @r.children.should == []
+      @s.children.should == []
     end
     should "keep track of their depth" do
       @p.depth.should == 0
       @q.depth.should == 1
       @r.depth.should == 2
+      @s.depth.should == 2
     end
 
     should "have correct paths" do
       @p.path.should == "/"
       @q.path.should == "/#{@q.slug}"
       @r.path.should == "/#{@q.slug}/#{@r.slug}"
+      @s.path.should == "/#{@q.slug}/#{@s.slug}"
     end
 
     should "all have a reference to the root node" do
@@ -88,6 +95,7 @@ class PageTest < Test::Unit::TestCase
       @p.root.should === @p
       @q.root.should === @p
       @r.root.should === @p
+      @s.root.should === @p
     end
   end
 end
