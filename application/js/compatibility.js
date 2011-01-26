79e670a18a4eecb4ecb5eae0ab16a7a1e60db360
Garry Hill <garry@magnetised.info>
2011-01-26 18:14:53 +0000

    added fix for Mozilla's renaming of window.createBlobUrl

--- application/js/compatibility.js
+++ application/js/compatibility.js
@@ -3,3 +3,9 @@
 // FormData
 // XHR.upload
 //
+if (window.URL && typeof window.URL.createObjectURL === "function") {
+	// why does FireFox always have to find a different name for everything?
+	window.createBlobURL = function(file) {
+		return window.URL.createObjectURL(file);
+	}
+}
