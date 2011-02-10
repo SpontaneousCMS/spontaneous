8245954bd36c08c58120cc4dbb86f807ca9e40db
Garry Hill <garry@magnetised.info>
2011-02-10 18:25:11 +0000

    added user_level.yml file to various locations & the site generator

--- test/unit/test_generators.rb
+++ test/unit/test_generators.rb
@@ -45,7 +45,7 @@ class GeneratorsTest < Test::Unit::TestCase
       %w(development.rb production.rb).each do |f|
         assert_file_exists(site_root, 'config/environments', f)
       end
-      %w(back.ru front.ru boot.rb database.yml deploy.rb environment.rb).each do |f|
+      %w(back.ru front.ru boot.rb database.yml deploy.rb environment.rb user_levels.yml).each do |f|
         assert_file_exists(site_root, 'config', f)
       end
       %w(favicon.ico robots.txt).each do |f|
