47f5a23f674c0f57dca6cf2a924d2bba1dc6c0c9
Garry Hill <garry@magnetised.info>
2011-02-19 10:51:15 +0000

    Correct calculation of number of bytes to generate for a particular length of random string

--- lib/spontaneous/permissions.rb
+++ lib/spontaneous/permissions.rb
@@ -45,9 +45,8 @@ module Spontaneous
       protected(:active_user=)
 
       def random_string(length)
-        # can't be bothered to work out the real rules behind this
-        bytes = (length.to_f / 1.375).ceil + 2
-        string = Base58.encode(OpenSSL::Random.random_bytes(bytes).unpack("h*").first.to_i(16)) #=> 44 chars
+        bytes = ((length * Math.log10(58))/(8 * Math.log10(2))).ceil + 2
+        string = Base58.encode(OpenSSL::Random.random_bytes(bytes).unpack("h*").first.to_i(16))
         string[0...(length)]
       end
 
