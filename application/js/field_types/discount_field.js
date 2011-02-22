f2f2726ac0876038c1b77b32f1a53be38a85e48e
Garry Hill <garry@magnetised.info>
2011-02-22 17:09:59 +0000

    started on markdown editor

--- application/js/field_types/discount_field.js
+++ application/js/field_types/discount_field.js
@@ -1,13 +1,149 @@
 console.log('Loading DiscountField...')
 Spontaneous.FieldTypes.DiscountField = (function($, S) {
 	var dom = S.Dom;
+	var TextCommand = new JS.Class({
+		name: '',
+		pre: '',
+		post: '',
+
+		extend: {
+			get_state: function(input) {
+				var start = input[0].selectionStart, end = input[0].selectionEnd, value = $(input).val(),
+				before = value.substr(0, start), middle = value.substr(start, (end - start)), after = value.substr(end);
+				return {
+					start: start,
+					end: end,
+					before: before,
+					middle: middle,
+					selection: middle,
+					after: after
+				}
+			}
+		},
+
+		initialize: function(input) {
+			this.input = input;
+		},
+		execute: function() {
+			this.wrap();
+			return false;
+		},
+		wrap: function() {
+			var input = this.input, s = TextCommand.get_state(input), start = s.start, end = s.end,
+				before = s.before, middle = s.middle, after = s.after, wrapped;
+			if ((end - start) <= 0 ) { return; }
+			if (this.matches_selection(middle)) {
+				wrapped  = this.remove(middle)
+			} else {
+				wrapped = this.surround(middle);
+			}
+			input.val(before + wrapped + after);
+			input[0].selectionStart = start;
+			input[0].selectionEnd = start + wrapped.length;
+		},
+		surround: function(text) {
+			return this.pre + text + this.post;
+		},
+		remove: function(text) {
+			return text.substr(this.pre.length, text.length - this.pre.length - this.post.length);
+		},
+		value: function() {
+			return this.input.val();
+		},
+		button: function() {
+			if (!this._button) {
+				var b = $(dom.a, {'class':this.name}).click(this.execute.bind(this)).text(this.name);
+				this._button = b;
+			}
+			return this._button;
+		},
+		respond_to_selection: function(state) {
+			if (this.matches_selection(state.selection)) {
+				console.log('> selected', this.name, state)
+			}
+		},
+		matches_removal: function(selection) {
+			return this.matches_selection(selection);
+		},
+		matches_selection: function(selection) {
+			return (selection.indexOf(this.pre) === 0 && selection.lastIndexOf(this.post) === (selection.length - this.post.length))
+		}
+	});
+	var Bold = new JS.Class(TextCommand, {
+		name: 'bold',
+		pre: '**',
+		post: '**'
+	});
+	var Italic = new JS.Class(TextCommand, {
+		name: 'italic',
+		pre: '_',
+		post: '_'
+	});
+	var Header = new JS.Class(TextCommand, {
+		pre: '',
+		post: "=",
+		scale: 1.0,
+		surround: function(text) {
+			if (this.matches_removal(text)) {
+				text = this.remove(text);
+			}
+			var line = '', n = Math.max(text.length, 30), newline = /([\r\n]+)$/, newlines = newline.exec(text), undef;
+			newlines = (!newlines || (newlines === undef) ? "" : newlines[1])
+			for (var i = 0; i < n; i++) { line += this.post; }
+			return text.replace(newline, '') + "\n" + line + newlines;
+		},
+		remove: function(text) {
+			var r = new RegExp('[\r\n][=-]+'), s =  text.replace(r, '')
+			return s.replace(/ +$/, '');
+		},
+		matches_removal: function(selection) {
+			return (new RegExp('[\r\n][=\\-]+[\r\n ]*$')).exec(selection)
+		},
+		matches_selection: function(selection) {
+			return (new RegExp('[\r\n]'+this.post+'+[\r\n ]*$')).exec(selection)
+		}
+	});
+
+	var H1 = new JS.Class(Header, {
+		name: "H1"
+	});
+	var H2 = new JS.Class(Header, {
+		name: "H2",
+		post: "-",
+		scale: 1.5,
+	});
 	var DiscountField = new JS.Class(Spontaneous.FieldTypes.StringField, {
+		actions: [Bold, Italic, H1, H2],
 		get_input: function() {
 			this.input = $(dom.textarea, {'id':this.css_id(), 'name':this.form_name(), 'rows':10, 'cols':30}).text(this.unprocessed_value());
 			return this.input;
 		},
 		edit: function() {
-			return this.get_input();
+			this._wrapper = $(dom.div, {'class':'markdown-editor', 'id':'editor-'+this.css_id()});
+			this._toolbar = $(dom.div, {'class':'md-toolbar'});
+			this.input = $(dom.textarea, {'id':this.css_id(), 'name':this.form_name(), 'rows':10, 'cols':30}).text(this.unprocessed_value());
+			this.input.select(function() {
+				this.on_select();
+			}.bind(this))
+			var c = this.actions;
+		this.commands = [];
+			for (var i = 0, ii = c.length; i < ii; i++) {
+				var cmd_class = c[i];
+				var cmd = new cmd_class(this.input);
+				this.commands.push(cmd);
+				this._toolbar.append(cmd.button());
+			}
+			this._wrapper.append(this._toolbar).append(this.input)
+			return this._wrapper;
+		},
+		on_select: function() {
+			var state = TextCommand.get_state(this.input);
+			$.each(this.commands, function() {
+				this.respond_to_selection(state);
+			});
+		},
+		bold: function() {
+			(new Bold(this.input)).execute();
 		}
 	});
 
--- test/ui/test_page_editing.rb
+++ test/ui/test_page_editing.rb
@@ -24,4 +24,54 @@ class PageEditingTest < Test::Unit::TestCase
       @browser.text('css=#page-info h1').should == new_title
     end
   end
+
+  context "Markdown text editor" do
+    setup do
+      @original_text = <<-ORIGINAL
+This is line 1.
+
+This is line 2. And this is line 2 too.
+      ORIGINAL
+      @browser.open("/@spontaneous#/1@edit")
+      @browser.wait_for_element("css=#page-fields")
+      @browser.click("css=#page-fields")
+      @browser.wait_for_element('css=#field-description-1')
+      @browser.type('name=field[description][unprocessed_value]', @original_text)
+    end
+
+    should "wrap selected text in bold delimiters" do
+      _start = @original_text.index('line 1')
+      _end   = _start + 6
+      @browser.get_eval(<<-JS)
+        var el = window.document.getElementById('field-description-1');
+        el.selectionStart = #{_start};
+        el.selectionEnd = #{_end};
+      JS
+      @browser.click("css=#editor-field-description-1 .md-toolbar a.bold")
+      # @browser.wait_for_element("css=#nontingnhgd")
+      @browser.value("css=#field-description-1").should == (<<-TXT).strip
+This is __line 1__.
+
+This is line 2. And this is line 2 too.
+      TXT
+      # puts @browser.get_eval('window.document.getElementById("field-description-1").selectionStart')
+      # puts @browser.get_eval('window.document.getElementById("field-description-1").selectionEnd')
+    end
+
+    should "not add bold markers around empty selection" do
+      _start = @original_text.index('line 1')
+      _end   = _start + 6
+      @browser.get_eval(<<-JS)
+        var el = window.document.getElementById('field-description-1');
+        el.selectionStart = 0
+        el.selectionEnd = 0
+      JS
+      @browser.click("css=#editor-field-description-1 .md-toolbar a.bold")
+      @browser.value("css=#field-description-1").should == (<<-TXT).strip
+This is line 1.
+
+This is line 2. And this is line 2 too.
+      TXT
+    end
+  end
 end
