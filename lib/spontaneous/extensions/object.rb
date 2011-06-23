84065adea7ae0d97c538d9dbd68297d6b5f53904
Garry Hill <garry@magnetised.info>
2011-06-23 10:15:11 +0100

    added missing Object.full_const_get from padrino

--- lib/spontaneous/extensions/object.rb
+++ lib/spontaneous/extensions/object.rb
@@ -10,9 +10,21 @@ module Spontaneous
 
       def meta
         @_meta ||= class << self; self; end
-      end
     end
+
+    def full_const_get(name)
+      list = name.split("::")
+      list.shift if list.first.blank?
+      obj = self
+      list.each do |x|
+        # This is required because const_get tries to look for constants in the
+        # ancestor chain, but we only want constants that are HERE
+        obj = obj.const_defined?(x) ? obj.const_get(x) : obj.const_missing(x)
+      end
+      obj
+    end unless method_defined?(:full_const_get)
   end
+end
 end
 
 
