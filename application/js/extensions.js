a82caa085cedbd615490b0cbc8cec79a81e5d5da
Garry Hill <garry@magnetised.info>
2011-07-18 10:59:44 +0100

    tidied extensions script

--- application/js/extensions.js
+++ application/js/extensions.js
@@ -24,43 +24,44 @@ if (!window.console) {
 
 // Thank you Prototype
 function $A(iterable) {
-  if (!iterable) return [];
-  if (iterable.toArray) return iterable.toArray();
-  var length = iterable.length || 0, results = new Array(length);
-  while (length--) results[length] = iterable[length];
-  return results;
+	if (!iterable) return [];
+	if (iterable.toArray) return iterable.toArray();
+	var length = iterable.length || 0, results = new Array(length);
+	while (length--) results[length] = iterable[length];
+	return results;
 };
 
-var function_id = 0;
-
-jQuery.extend(Function.prototype, {
-	bind: function() {
-		var __method = this, args = $A(arguments), object = args.shift();
-		return function() {
-      return __method.apply(object, args.concat($A(arguments)));
-    };
-	},
-	cache: function(name) {
-		var __method = this, id = name || ('__cached__'+(++function_id)), _undefined_;
-		return function() {
-			if (this[id] === _undefined_) {
-				this[id] =	__method.apply(this, arguments) || null;
-			}
-			return this[id];
-		};
-	}
-});
-
-
-jQuery.extend(Number.prototype, {
-	to_filesize: function() {
-		var thou = 1000, units = [" bytes", ' KB', ' MB', ' GB'],
-		power = Math.floor(Math.log(this) / Math.log(thou))
-		return this / (Math.pow(thou, power)) + units[power]
-	}
-});
-
 (function($) {
+
+	var function_id = 0;
+
+	$.extend(Function.prototype, {
+		bind: function() {
+			var __method = this, args = $A(arguments), object = args.shift();
+			return function() {
+				return __method.apply(object, args.concat($A(arguments)));
+			};
+		},
+		cache: function(name) {
+			var __method = this, id = name || ('__cached__'+(++function_id)), _undefined_;
+			return function() {
+				if (this[id] === _undefined_) {
+					this[id] = __method.apply(this, arguments) || null;
+				}
+				return this[id];
+			};
+		}
+	});
+
+
+	$.extend(Number.prototype, {
+		to_filesize: function() {
+			var thou = 1000, units = [" bytes", ' KB', ' MB', ' GB'],
+			power = Math.floor(Math.log(this) / Math.log(thou))
+			return this / (Math.pow(thou, power)) + units[power]
+		}
+	});
+
 	var opacity_change_duration = 200, height_change_duration = 200;
 	$.fn.appear = function(callback) {
 		var $this = this;
