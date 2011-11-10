7efbe4b52e30f7ef6c8b32ed3c131a30d0f0b0e9
Garry Hill <garry@magnetised.info>
2011-11-10 14:59:27 +0000

    Add Box::owner_sid

--- lib/spontaneous/box.rb
+++ lib/spontaneous/box.rb
@@ -54,9 +54,9 @@ module Spontaneous
       !supertype.nil? #&& supertype.respond_to?(:field_prototypes)
     end
 
-    # def self.owner_sid
-    #   nil
-    # end
+    def self.owner_sid
+      nil
+    end
 
     def page?
       false
