a26a28c0fa4b16a6372807e249af335cc42cc032
Garry Hill <garry@magnetised.info>
2011-11-18 18:41:11 +0000

    scripts helper now accepts an array

--- lib/spontaneous/render/helpers/script_helper.rb
+++ lib/spontaneous/render/helpers/script_helper.rb
@@ -3,7 +3,7 @@
 module Spontaneous::Render::Helpers
   module ScriptHelper
     def script(*args)
-      scripts = args
+      scripts = args.flatten
       scripts.map do |script|
         %(<script type="text/javascript" src="#{script}"></script>)
       end.join("\n")
