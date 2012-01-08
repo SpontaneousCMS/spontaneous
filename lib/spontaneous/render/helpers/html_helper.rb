a2c4bc2969ff00776d7b63f124e780500ffe80d0
Garry Hill <garry@magnetised.info>
2012-01-08 19:00:06 +0000

    Add helpers for IE conditional comments

--- lib/spontaneous/render/helpers/html_helper.rb
+++ lib/spontaneous/render/helpers/html_helper.rb
@@ -13,5 +13,57 @@ module Spontaneous::Render::Helpers
       return "" if args.empty?
       %(class="#{args.join(" ")}")
     end
+
+    def ie_only(version = nil)
+      ie_conditional_comment(version)
+    end
+
+    def ie_only_gt(version)
+      ie_conditional_comment(version, "gt")
+    end
+
+    def ie_only_gte(version)
+      ie_conditional_comment(version, "gte")
+    end
+
+    def ie_only_lt(version)
+      ie_conditional_comment(version, "lt")
+    end
+
+    def ie_only_lte(version)
+      ie_conditional_comment(version, "lte")
+    end
+
+    def ie_only_end
+      "<![endif]-->"
+    end
+
+    def not_ie
+       "<!--[if !IE]> -->"
+    end
+
+    def not_ie_end
+       "<!-- <![endif]-->"
+    end
+
+    def ie_conditional_comment(version = nil, test = nil)
+      ie_conditional_comment_start(version, test)
+    end
+
+    def ie_conditional_comment_start(version = nil, test = nil)
+      ["<!--[if ", test_for_version(version, test), "]>"].join
+    end
+
+    def test_for_version(version, test)
+      if version.is_a?(Range)
+        version = version.to_a
+        lower = "(#{test_for_version(version.first, "gte")})"
+        upper = "(#{test_for_version(version.last,  "lte")})"
+        [lower, upper].join("&")
+      else
+        [test, "IE", version].compact.join(" ")
+      end
+    end
+
   end
 end
--- test/unit/test_helpers.rb
+++ test/unit/test_helpers.rb
@@ -28,5 +28,27 @@ class HelpersTest < MiniTest::Spec
       classes = @helper.classes("a b c", :active => false, :invisible => true)
       classes.should == %(class="a b c invisible")
     end
+
+    should "provide a wrapper around IE conditional comments" do
+      @helper.ie_only.should == "<!--[if IE]>"
+      @helper.ie_only(6).should == "<!--[if IE 6]>"
+      @helper.ie_only(7).should == "<!--[if IE 7]>"
+      @helper.ie_only_gt(7).should == "<!--[if gt IE 7]>"
+      @helper.ie_only_gte(7).should == "<!--[if gte IE 7]>"
+      @helper.ie_only_gte(8).should == "<!--[if gte IE 8]>"
+      @helper.ie_only_lt(7).should == "<!--[if lt IE 7]>"
+      @helper.ie_only_lte(9).should == "<!--[if lte IE 9]>"
+      @helper.ie_only_end.should == "<![endif]-->"
+    end
+
+    should "enable targeting a range of ie versions using ranges" do
+      @helper.ie_only(6..8).should == "<!--[if (gte IE 6)&(lte IE 8)]>"
+      @helper.ie_only(6...8).should == "<!--[if (gte IE 6)&(lte IE 7)]>"
+    end
+
+    should "provide a wrapper around only comments excluding IE" do
+      @helper.not_ie.should == "<!--[if !IE]> -->"
+      @helper.not_ie_end.should == "<!-- <![endif]-->"
+    end
   end
 end
