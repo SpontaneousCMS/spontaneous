29b99c762979f96ded9a107b9b2a2d0125484c53
Garry Hill <garry@magnetised.info>
2012-01-19 11:48:32 +0000

    Release EntrySet from its Array limits

--- lib/spontaneous/collections/entry_set.rb
+++ lib/spontaneous/collections/entry_set.rb
@@ -2,15 +2,18 @@
 
 
 module Spontaneous::Collections
-  class EntrySet < Array
-    alias_method :array_insert, :insert
-    alias_method :array_push,   :push
-    alias_method :array_delete, :delete
+  class EntrySet
+    extend  Forwardable
+    include Enumerable
 
-    attr_reader :owner
+    attr_reader :owner, :store
+
+
+    def_delegators :@store, :each, :[], :index, :<<, :length, :last
 
     def initialize(owner, piece_store = [])
       @owner = owner
+      @store = []
       (piece_store || []).each do |data|
         id = data[0]
         entry = \
@@ -26,18 +29,22 @@ module Spontaneous::Collections
           end
         # if the piece/page has been deleted or is invisible
         # then we just want to silently skip it
-        array_push(entry) if entry
+        @store << entry if entry
       end
     end
 
     def for_box(box)
       sid = box.schema_id
-      self.select { |e| e.box_sid == sid }
+      store.select { |e| e.box_sid == sid }
     end
 
     def insert(index, piece)
       # piece.piece_store = self
-      array_insert(index, piece)
+
+      # puts "array_insert #{piece.serialize_db} => #{index}"
+      # p serialize_db
+      store.insert(index, piece)
+      # p serialize_db
       owner.entry_modified!(piece)
     end
 
@@ -48,7 +55,7 @@ module Spontaneous::Collections
     end
 
     def destroy
-      self.dup.each { |e| e.destroy(false) }
+      store.dup.each { |e| e.destroy(false) }
     end
 
     def remove(piece)
@@ -57,40 +64,53 @@ module Spontaneous::Collections
 
 
     def delete(piece)
-      e = array_delete(piece)
+      e = store.delete(piece)
       owner.entry_modified!(nil)
     end
 
     def serialize_db
-      self.map { |e| e.serialize_db }
+      store.map { |e| e.serialize_db }
     end
 
 
     def set_position(content, position)
-      piece = self.detect {|e| e.id == content.id }
-      self.array_delete(piece)
-      self.insert(position, piece)
+      piece = store.detect {|e| e.id == content.id }
+      store.delete(piece)
+      store.insert(position, piece)
       owner.entry_modified!(piece)
     end
 
     def export
-      map do | piece |
+      store.map do | piece |
         piece.export
       end
     end
 
+    def freeze
+      super
+      store.freeze
+      self
+    end
+
     # Returns a frozen version of this set containing only entries that are visible
     def visible!
-      entries = self.dup.reject { |e|  e.nil? || e.hidden? }
+      entries = store.dup.reject { |e|  e.nil? || e.hidden? }
       # In 1.9.3 #reject does not return this subclass of Array but instead
       # returns a direct Array instance
-      if Array === entries
-        set = EntrySet.new(owner)
-        set.concat(entries).freeze
-        set
-      else
-        entries
-      end
+      set = EntrySet.new(owner)
+      set.concat(entries).freeze
+      set
+    end
+
+    def ==(set)
+      super or store == set
+    end
+
+    protected
+
+    def concat(entries)
+      store.concat(entries)
+      self
     end
   end
 end
