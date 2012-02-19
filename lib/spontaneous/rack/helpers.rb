c14a5784074d49fa87d21b0bb8a8b35ec5ea2fca
Garry Hill <garry@magnetised.info>
2012-02-19 12:08:06 +0000

    Remove env test for scripts -- bundling etc should be done by gem creation process

--- lib/spontaneous/rack/helpers.rb
+++ lib/spontaneous/rack/helpers.rb
@@ -13,7 +13,7 @@ module Spontaneous
       end
 
       def script_list(scripts)
-        if Spontaneous.development?
+        # if Spontaneous.development?
           scripts.map do |script|
             src = "/js/#{script}.js"
             path = Spontaneous.application_dir(src)
@@ -21,9 +21,9 @@ module Spontaneous
             ["#{NAMESPACE}#{src}", size]
             # %(<script src="#{NAMESPACE}/js/#{script}.js" type="text/javascript"></script>)
           end.to_json
-        else
+        # else
           # script bundling + compression
-        end
+        # end
       end
     end
   end
