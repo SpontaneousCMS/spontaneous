41dee3480ebcfe98a169c44991f289e48d0af6ee
Garry Hill <garry@magnetised.info>
2012-04-13 13:49:39 +0100

    Fixed calling of actions on the home page

--- lib/spontaneous/rack/public.rb
+++ lib/spontaneous/rack/public.rb
@@ -186,11 +186,11 @@ module Spontaneous
       def parse_path(path)
         if path =~ %r(#{ACTION})
           path, action = path.split(ACTION)
+          path = "/" if path.empty?
           action, format = action.split(DOT)
         else
           path, format = path.split(DOT)
         end
-        # format = (format || :html).to_sym
         [path, format, action]
       end
 
