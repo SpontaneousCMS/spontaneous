0f02f2546c1ecfd577b7bc9160147816db418567
Garry Hill <garry@magnetised.info>
2012-07-06 14:46:32 +0100

    Comment out un-used Sass integration because of namespace conflicts

--- lib/spontaneous/asset.rb
+++ lib/spontaneous/asset.rb
@@ -52,24 +52,24 @@ module Spontaneous
     end
   end
 end
-module Spontaneous
-  module Sass
-    module Helpers
-      def image_url(asset)
-        ::Sass::Script::String.new "url(" + resolver.image_path(asset.value) + ")"
-      end
-
-      def resolver
-        p options#[:quiet]
-        options[:custom][:resolver]
-      end
-    end
-  end
-end
-module Sass
-  module Script
-    module Functions
-      include Spontaneous::Sass::Helpers
-    end
-  end
-end
+# module Spontaneous
+#   module Sass
+#     module Helpers
+#       def image_url(asset)
+#         ::Sass::Script::String.new "url(" + resolver.image_path(asset.value) + ")"
+#       end
+#
+#       def resolver
+#         p options#[:quiet]
+#         options[:custom][:resolver]
+#       end
+#     end
+#   end
+# end
+# module Sass
+#   module Script
+#     module Functions
+#       include Spontaneous::Sass::Helpers
+#     end
+#   end
+# end
