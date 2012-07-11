704877e33da1ce70f0207a7cc71de148faf73cf6
Garry Hill <garry@magnetised.info>
2012-07-11 17:02:57 +0100

    Share upload cache & tmp directory between releases

--- lib/spontaneous/capistrano/deploy.rb
+++ lib/spontaneous/capistrano/deploy.rb
@@ -5,25 +5,27 @@ Capistrano::Configuration.instance(:must_exist).load do
   set :bundle_cmd,              "bundle"
   set :bundle_flags,            "--deployment --quiet --binstubs --shebang ruby-local-exec"
 
+  # Remove tmp/pids from list of shared dirs that get symlinked into the release
+  set :shared_children,   %w(public/system log)
+
   set :media_dir,   lambda { "#{deploy_to}/media" }
   set :revision_dir, lambda { "#{deploy_to}/revisions" }
-
-  # namespace :rvm do
-  #   task :trust_rvmrc do
-  #     run "if [ -f #{release_path}/.rvmrc ];then rvm rvmrc trust #{release_path}; fi"
-  #   end
-  # end
-  #
-  # after "deploy", "rvm:trust_rvmrc"
+  set :upload_dir, lambda { "#{deploy_to}/uploadcache" }
 
   namespace :spot do
     task :symlink_cache do
       cache_dir = File.join(latest_release, 'cache')
-      run "mkdir #{cache_dir}; ln -s #{deploy_to}/media #{cache_dir}; ln -s #{deploy_to}/revisions #{cache_dir}"
+      run "mkdir #{cache_dir}; ln -s #{deploy_to}/media #{cache_dir}; ln -s #{deploy_to}/revisions #{cache_dir}; ln -s #{deploy_to}/uploadcache #{cache_dir}/tmp"
     end
 
     task :symlink_application do
       run "cd #{release_path} && ln -s `bundle show spontaneous`/application public/.spontaneous"
+    end
+
+    # Capistrano automatically creates a tmp directory - I don't like that
+    # and would prefer to share tmp between instances
+    task :symlink_tmpdir do
+      run "cd #{release_path} && rmdir tmp ; ln -nfs #{deploy_to}/shared/tmp ."
     end
 
     task :bundle_assets do
@@ -39,6 +41,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   end
 
   after 'deploy:finalize_update', 'spot:symlink_cache'
+  after 'deploy:finalize_update', 'spot:symlink_tmpdir'
   after 'bundle:install', 'spot:symlink_application'
   after 'bundle:install', 'spot:bundle_assets'
 end
