7223fbda1e0bf42ca93be09f35219a760e08eeba
Garry Hill <garry@magnetised.info>
2012-07-11 16:30:20 +0100

    Fix for capistrano's deploy:cleanup

--- lib/spontaneous/capistrano.rb
+++ lib/spontaneous/capistrano.rb
@@ -2,3 +2,12 @@
 %w(deploy sync).each do |task|
   load File.expand_path("../capistrano/#{task}.rb", __FILE__)
 end
+
+# Fix for ActiveSupport breaking Capistrano's #capture method.
+# See this thread:
+# https://github.com/capistrano/capistrano/issues/168
+Capistrano::Configuration::Namespaces::Namespace.class_eval do
+  def capture(*args)
+    parent.capture *args
+  end
+end
