04261a79a43ec9cd45f14d04fcd22885cdfd85f5
Garry Hill <garry@magnetised.info>
2012-07-23 18:18:42 +0100

    Make Array instances implement the #render_using API

--- lib/spontaneous/extensions/array.rb
+++ lib/spontaneous/extensions/array.rb
@@ -3,6 +3,18 @@
 module Spontaneous
   module Extensions
     module Array
+      def render_using(renderer, *args)
+        self.map { |e|
+          if e.respond_to?(:render_using)
+            e.render_using(renderer, *args)
+          elsif e.respond_to?(:render)
+            e.render(*args)
+          else
+            e
+          end
+        }.join
+      end
+
       def render(*args)
         self.map { |e| e.respond_to?(:render) ? e.render(*args) : e }.join
       end
