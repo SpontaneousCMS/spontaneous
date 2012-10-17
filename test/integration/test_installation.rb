3c4d8df9c5cb605b1fa7caadda14c511c0b55847
Garry Hill <garry@magnetised.info>
2012-10-17 08:48:10 +0100

    Sorting out my requires to get basic installation test to run

--- test/integration/test_installation.rb
+++ test/integration/test_installation.rb
@@ -2,11 +2,30 @@
 
 require File.expand_path('../../test_integration_helper', __FILE__)
 
-class InstallationTest < MiniTest::Spec
-  context "installation" do
-    setup do
-      @site = setup_site
-      Dir.chdir(@site.root)
+describe "Installation" do
+  def system(command)
+    puts "SYSTEM #{command.inspect}"
+    Kernel.system command
+  end
+
+  before do
+    @root = Dir.mktmpdir
+    Dir.chdir(@root)
+    puts "root.before"
+  end
+
+  describe "when starting with a base system" do
+    before do
+      puts "describe.before"
+    end
+
+    it "will allow the installation of the spontaneous gem" do
+      assert_raises "Precondition failed, spontaneous gem is already installed", Gem::LoadError do
+        Gem::Specification.find_by_name("spontaneous")
+      end
+      system "gem install spontaneous --prerelease --no-rdoc --no-ri"
+      spec = Gem::Specification.find_by_name("spontaneous")
+      asssert_instance_of Gem::Specification, spec, "spontaneous gem should have been installed"
     end
   end
 end
--- test/test_integration_helper.rb
+++ test/test_integration_helper.rb
@@ -2,8 +2,8 @@
 
 # Helpers for integration tests
 require "rubygems"
-require "bundler"
-gem 'minitest'
-Bundler.setup(:default, :development)
-
-Bundler.require
+require "bundler/setup"
+require "minitest/spec"
+require "minitest/autorun"
+require 'tmpdir'
+require 'pp'
