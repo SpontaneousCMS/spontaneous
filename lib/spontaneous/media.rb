eee412b3b484651c4b62ee281bac20864c512656
Garry Hill <garry@magnetised.info>
2012-11-02 16:35:43 +0000

    Removing old unused code

--- lib/spontaneous/media.rb
+++ lib/spontaneous/media.rb
@@ -15,25 +15,6 @@ module Spontaneous
       ::File.join(Spontaneous.media_dir, *args)
     end
 
-    @@upload_uid_lock  = Mutex.new
-    @@upload_uid_index = 0
-
-    def upload_index
-      @@upload_uid_lock.synchronize do
-        @@upload_uid_index = (@@upload_uid_index + 1) % 0xFFFFFF
-      end
-    end
-
-    def upload_path(filename)
-      time = Time.now.to_i
-      dir = "#{time}.#{upload_index}"
-      Spontaneous.cache_path("tmp", dir, filename)
-    end
-
-    # def to_urlpath(filepath)
-    #   filepath.gsub(%r{^#{Spontaneous.media_dir}}, "/media")
-    # end
-
     def to_filepath(urlpath)
       parts = urlpath.split("/")
       parts[1] = Spontaneous.media_dir
