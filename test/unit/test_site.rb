60c470c6c45c4e43758f9ab513d86704278c72ab
Garry Hill <garry@magnetised.info>
2013-01-24 16:44:15 +0000

    Changes to page slugs should mark the site modified

--- test/unit/test_site.rb
+++ test/unit/test_site.rb
@@ -188,6 +188,14 @@ class SiteTest < MiniTest::Spec
         Site.modified_at.should == now
       end
 
+      should "be updated when a page's slug changes" do
+        root = ::Page.create
+        now = @now + 98
+        Time.stubs(:now).returns(now)
+        root.update(:slug => "updated-slug")
+        Site.modified_at.should == now
+      end
+
       should "not be updated when a piece is added" do
         now1 = @now + 24
         Time.stubs(:now).returns(now1)
