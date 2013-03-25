d00a68a52762a943aef690e600d282d8cc12cd36
Garry Hill <garry@magnetised.info>
2013-03-25 09:03:30 +0000

    Remove old provider implementation (phew)

--- lib/spontaneous/field/webvideo.rb
+++ lib/spontaneous/field/webvideo.rb
@@ -86,11 +86,6 @@ module Spontaneous::Field
       values[:id] || values[:video_id]
     end
 
-    def provider_id
-      values[:provider]
-    end
-
-
     def generate_outputs(input)
       values = {}
       values[:html] = escape_html(input)
@@ -99,26 +94,8 @@ module Spontaneous::Field
       Spontaneous::Field::WebVideo.providers.each do |_, p|
         break if metadata = p.match(self, input)
       end
-      # p metadata
-      # case input
-      # when "", nil
-      #   # ignore this
-      # when /youtube\\.com.*\\?.*v=([^&]+)/, /youtu.be\\/([^&]+)/
-      #   video_id = $1
-      #   values.update(retrieve_youtube_metadata(video_id))
-      #   values[:video_id] = video_id.to_s
-      #   values[:type] = "youtube"
-      # when /vimeo\\.com\\/(\\d+)/
-      #   video_id = $1
-      #   values[:type] = "vimeo"
-      #   values.update(retrieve_vimeo_metadata(video_id))
-      #   values[:video_id] = video_id.to_s
-      # else
-      #   logger.warn "WebVideo field doesn't recognise the URL '#{input}'"
-      # end
       values.merge(metadata || {})
     end
-
 
     def render(format=:html, *args)
       case format
@@ -131,16 +108,18 @@ module Spontaneous::Field
       end
     end
 
-
     def to_html(*args)
       provider.to_html(*args)
+    end
+
+    def provider_id
+      values[:provider]
     end
 
     def provider
       provider = Spontaneous::Field::WebVideo.providers[provider_id]
       provider.new(self, values)
     end
-
 
     def to_json(*args)
       opts = args.extract_options!
@@ -154,213 +133,6 @@ module Spontaneous::Field
     def ui_preview_value
       # render(:html, :width => 480, :height => 270)
       src
-    end
-
-    def retrieve_vimeo_metadata(video_id)
-      url = "http://vimeo.com/api/v2/video/%s.json" % video_id
-      response = \
-        begin
-          open(url).read
-      rescue => e
-        logger.error("Unable to retrieve metadata for video ##{video_id} from Vimeo: '#{e}'")
-        "[{}]"
-      end
-      metadata = Spontaneous.parse_json(response) rescue [{}]
-      metadata = metadata.first || {}
-    end
-
-    def retrieve_youtube_metadata(video_id)
-      url = "http://gdata.youtube.com/feeds/api/videos/%s?v=2" % video_id
-      begin
-        doc = Nokogiri::XML(open(url))
-        entry = doc.xpath("xmlns:entry")
-
-        { "title" => entry.xpath("xmlns:title").text.strip,
-          "description" => entry.xpath("media:group/media:description").text.strip,
-          "thumbnail_large" => entry.xpath('media:group/media:thumbnail[@yt:name="hqdefault"]').first["url"].strip,
-          "thumbnail_small" => entry.xpath('media:group/media:thumbnail[@yt:name="default"]').first["url"].strip,
-          "user_name" => entry.xpath("xmlns:author/xmlns:name").text.strip,
-          "upload_date" => Time.parse(entry.xpath("xmlns:published").text.strip).strftime("%Y-%m-%d %H:%M:%S"),
-          "tags" => entry.xpath("media:group/media:keywords").text.strip,
-          "duration" => entry.xpath("media:group/yt:duration").first['seconds'].to_i,
-          "stats_number_of_likes" => entry.xpath("yt:rating").first['numLikes'].to_i,
-          "stats_number_of_plays" => entry.xpath("yt:statistics").first['viewCount'].to_i,
-          "stats_number_of_comments" => entry.xpath("gd:comments/gd:feedLink").first['countHint'].to_i }
-      rescue
-        {}
-      end
-    end
-
-    def default_player_options
-      {
-        :width => 640,
-        :height => 360,
-        :fullscreen => true,
-        :api => false,
-        :autoplay => false,
-        :loop => false,
-        :showinfo => true
-      }.merge(prototype.options[:player] || {})
-    end
-
-
-    def to_vimeo_html(options = {})
-      params = vimeo_attributes(options)
-
-      attributes = make_html_attributes(params[:attr])
-      %(<iframe #{attributes}></iframe>)
-    end
-
-    def vimeo_attributes(options = {})
-
-      o = make_vimeo_options(options)
-
-      attributes = {
-        :type => "text/html",
-        :frameborder => "0",
-        :width => o.delete(:width),
-        :height => o.delete(:height)
-      }
-      attributes.update(:webkitAllowFullScreen => "yes", :allowFullScreen => "yes") if o[:fullscreen]
-
-      make_query_options!(o)
-      attributes[:src] = vimeo_src_with_options(o)
-
-      {:tagname => "iframe", :tag => "<iframe/>", :attr => attributes}
-    end
-
-    def make_vimeo_options(options = {})
-      o  = default_player_options
-      vimeo_options = o.delete(:vimeo) || {}
-
-      o.merge!(vimeo_options)
-
-      if o.delete(:showinfo)
-        o[:portrait] = true
-        o[:title] = true
-        o[:byline] = true
-      else
-        o[:portrait] = false
-        o[:title] = false
-        o[:byline] = false
-      end
-
-      vimeo_options = options.delete(:vimeo) || {}
-
-      o = {
-        :portrait => true,
-        :title => true,
-        :byline => true,
-        :player_id => "vimeo#{owner.id}id#{value(:video_id)}"
-      }.merge(o).merge(vimeo_options).merge(options)
-      o
-    end
-
-    def vimeo_src(options = {})
-      o = make_vimeo_options(options)
-      make_query_options!(o)
-      vimeo_src_with_options(o)
-    end
-
-    def vimeo_src_with_options(o)
-      params = {
-        "title" => o[:title],
-        "byline" => o[:byline],
-        "portrait" => o[:portrait],
-        "autoplay" => o[:autoplay],
-        "loop" => o[:loop],
-        "api" => o[:api],
-        "player_id" => o[:player_id] }
-        params.update("color" => o[:color]) if o.key?(:color)
-        params = ::Rack::Utils.build_query(params)
-        "http://player.vimeo.com/video/#{video_id}?#{params}"
-    end
-
-    def to_youtube_html(options = {})
-      params = youtube_attributes(options)
-      attributes = make_html_attributes(params[:attr])
-      %(<iframe #{attributes}></iframe>)
-    end
-
-
-    def youtube_attributes(options = {})
-      o = make_youtube_options(options)
-
-      attributes = {
-        :type => "text/html",
-        :frameborder => "0",
-        :width => o.delete(:width),
-        :height => o.delete(:height)
-      }
-
-      make_query_options!(o)
-
-      attributes[:src] = youtube_src_with_options(o)
-
-      attributes.update(:webkitAllowFullScreen => "yes", :allowFullScreen => "yes") if o[:fullscreen]
-
-      {:tagname => "iframe", :tag => "<iframe/>", :attr => attributes}
-    end
-
-    def make_youtube_options(options = {})
-      o  = default_player_options
-      youtube_options = o.delete(:youtube) || {}
-
-      o.merge!(youtube_options)
-
-      if o.delete(:showinfo)
-        o[:showinfo] = true
-        o[:showsearch] = true
-      else
-        o[:showinfo] = false
-        o[:showsearch] = false
-      end
-
-      youtube_options = options.delete(:youtube) || {}
-      o = {
-        :theme => "dark",
-        :hd => true,
-        :controls => true,
-        :showinfo => true,
-        :showsearch => true,
-        :autohide => 2,
-        :rel => true
-      }.merge(o).merge(youtube_options).merge(options)
-    end
-
-    def youtube_src(options = {})
-      o = make_youtube_options(options)
-      make_query_options!(o)
-      youtube_src_with_options(o)
-    end
-
-    def youtube_src_with_options(o)
-      params = ::Rack::Utils.build_query({
-        "modestbranding" => 1,
-        "theme" => o[:theme],
-        "hd" => o[:hd],
-        "fs" => o[:fullscreen],
-        "controls" => o[:controls],
-        "autoplay" => o[:autoplay],
-        "showinfo" => o[:showinfo],
-        "showsearch" => o[:showsearch],
-        "loop" => o[:loop],
-        "autohide" => o[:autohide],
-        "rel" => o[:rel],
-        "enablejsapi" => o[:api] })
-        "http://www.youtube.com/embed/#{video_id}?#{params}"
-    end
-
-    def make_html_attributes(attributes)
-      attributes.to_a.map { |name, value| "#{name}=\"#{escape_html(value)}\"" }.join(" ")
-    end
-
-
-    def make_query_options!(hash)
-      hash.each { |k, v|
-        hash[k] = 1 if v == true
-        hash[k] = 0 if v == false
-      }
     end
 
     self.register(:webvideo)
