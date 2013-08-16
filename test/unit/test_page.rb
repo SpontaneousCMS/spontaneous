1c34515d5ba562d052066c4ff036a954d60b39f9
Garry Hill <garry@magnetised.info>
2013-08-16 13:46:26 +0100

    Shorten max length of slugs & break on word boundaries
    255 chars was just too long.

--- test/unit/test_page.rb
+++ test/unit/test_page.rb
@@ -179,12 +179,27 @@ describe "Page" do
       p.path.must_equal "/my-slug-01"
     end
 
-    it "not be longer than 255 chars" do
+    it "not be longer than 64 chars" do
       o = Page.create
       long_slug = (["bang"]*100)
       o.slug = long_slug.join(' ')
-      o.slug.length.must_equal 255
-      o.slug.must_equal long_slug.join('-')[0..254]
+      o.slug.length.must_equal 64
+      o.slug.must_equal long_slug.join('-')[0..63]
+    end
+
+    it "should crop titles at word boundaries" do
+      o = Page.create
+      long_slug = (["bangor"]*100)
+      expected = %w(bangor bangor bangor bangor bangor bangor bangor bangor bangor).join('-')
+      o.slug = long_slug.join(' ')
+      o.slug.length.must_equal expected.length
+      o.slug.must_equal expected
+    end
+
+    it "should just crop a very long word to the max length" do
+      o = Page.create
+      o.slug = "a"*100
+      o.slug.length.must_equal 64
     end
   end
 
