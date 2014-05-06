04a7219e57edf0e9a8f28a788c07cab2b64b5b76
Garry Hill <garry@magnetised.net>
2014-05-06 18:08:35 +0100

    Mysql only allows indexes up to 767 bytes

--- db/migrations/20130813111009_increase_path_length.rb
+++ db/migrations/20130813111009_increase_path_length.rb
@@ -1,7 +1,17 @@
 Sequel.migration do
+
   up do
+    length = case self.adapter_scheme
+    when :mysql, :mysql2
+      # http://stackoverflow.com/questions/1814532/1071-specified-key-was-too-long-max-key-length-is-767-bytes/1814594#1814594
+      # reasons to move away from mysql...
+      255
+    else
+      2048
+    end
+
     [:content, :spontaneous_content_archive, :spontaneous_content_history].each do |table|
-      set_column_type table, :path, 'varchar(2048)'
+      set_column_type table, :path, "varchar(#{length})"
     end
   end
 
@@ -11,4 +21,3 @@ Sequel.migration do
     end
   end
 end
-
