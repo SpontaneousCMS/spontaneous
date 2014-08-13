13726ed6e83f127c45e16fb7ef165d76b82027f7
Garry Hill <garry@magnetised.net>
2014-08-13 15:43:40 +0100

    Rescue duplicate column errors in migration Works around the case where the production db is old and db syncs puts the db into an unusual state where the content_archive table is a couple of migrations ahead of the rest of the db

--- db/migrations/20140514090204_add_content_hash.rb
+++ db/migrations/20140514090204_add_content_hash.rb
@@ -1,12 +1,21 @@
 # encoding: UTF-8
 
 Sequel.migration do
+  no_transaction
   up do
-    [:content, :spontaneous_content_archive, :spontaneous_content_history].each do |table|
-      alter_table table do
-        add_column :content_hash, :varchar, size: 32
-        add_column :published_content_hash, :varchar, size: 32
-        add_column :content_hash_changed, :boolean, default: true
+    [:content, :spontaneous_content_history, :spontaneous_content_archive].each do |table|
+      begin
+        alter_table table do
+          add_column :content_hash, :varchar, size: 32
+          add_column :published_content_hash, :varchar, size: 32
+          add_column :content_hash_changed, :boolean, default: true
+        end
+      rescue ::Sequel::DatabaseError => e
+        # because syncing ignores the content_archive table
+        # if we run this migration, then sync, then re-run it
+        # (in the case where the production db is behind the dev db)
+        # this one table will throw an error
+        raise unless table == :spontaneous_content_archive
       end
     end
     alter_table :content do
@@ -15,32 +24,34 @@ Sequel.migration do
 
     # testing environment
     if defined?(Content)
+      transaction do
 
-      self.logger = nil
+        self.logger = nil
 
-      state = Spontaneous::State.first
+        state = Spontaneous::State.first
 
-      published_revision = state.nil? ? nil : state[:published_revision]
+        published_revision = state.nil? ? nil : state[:published_revision]
 
-      content_hash = published_content_hash = nil
+        content_hash = published_content_hash = nil
 
-      model = Spontaneous::Model(:content)
+        model = Spontaneous::Model(:content)
 
-      update_content_hashes = Proc.new do |content|
-        published_content_hash = nil
-        content_hash = content.calculate_content_hash!
-        if published_revision
-          model.mapper.scope(published_revision, false) do
-            published = model[content.id]
-            published_content_hash = published.calculate_content_hash! if published
+        update_content_hashes = Proc.new do |content|
+          published_content_hash = nil
+          content_hash = content.calculate_content_hash!
+          if published_revision
+            model.mapper.scope(published_revision, false) do
+              published = model[content.id]
+              published_content_hash = published.calculate_content_hash! if published
+            end
           end
+          p [content.id, content.class, content.path, content_hash, published_content_hash] #if published_content_hash != content_hash
+          model.dataset.unfiltered.where(id: content.id).update(content_hash: content_hash, published_content_hash: published_content_hash, content_hash_changed: (content_hash != published_content_hash))
         end
-        p [content.id, content.class, content.path, content_hash, published_content_hash] #if published_content_hash != content_hash
-        model.dataset.unfiltered.where(id: content.id).update(content_hash: content_hash, published_content_hash: published_content_hash, content_hash_changed: (content_hash != published_content_hash))
-      end
 
-      Content::Piece.dataset.order(Sequel.desc(:depth)).each(&update_content_hashes)
-      Content::Page.dataset.order(Sequel.desc(:depth)).each(&update_content_hashes)
+        Content::Piece.dataset.order(Sequel.desc(:depth)).each(&update_content_hashes)
+        Content::Page.dataset.order(Sequel.desc(:depth)).each(&update_content_hashes)
+      end
     end
   end
 
--- db/migrations/20140519150253_add_content_hash_timestamp.rb
+++ db/migrations/20140519150253_add_content_hash_timestamp.rb
@@ -1,12 +1,23 @@
 # encoding: UTF-8
 
 Sequel.migration do
+  no_transaction
   up do
     [:content, :spontaneous_content_archive, :spontaneous_content_history].each do |table|
-      alter_table table do
-        add_column :content_hash_changed_at, :timestamp
+      begin
+        alter_table table do
+          add_column :content_hash_changed_at, :timestamp
+        end
+      rescue ::Sequel::DatabaseError => e
+        # because syncing ignores the content_archive table
+        # if we run this migration, then sync, then re-run it
+        # (in the case where the production db is behind the dev db)
+        # this one table will throw an error
+        raise unless table == :spontaneous_content_archive
       end
-      self[table].update(content_hash_changed_at: :modified_at)
+      transaction do
+        self[table].update(content_hash_changed_at: :modified_at)
+      end
     end
   end
 
