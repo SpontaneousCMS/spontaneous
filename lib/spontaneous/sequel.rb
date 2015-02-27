6ba0dcba0a26bc1b0d20313971603b79cb3b4f9e
Garry Hill <garry@magnetised.net>
2015-02-27 09:06:28 +0000

    Set Sequel's timezones to UTC

--- lib/spontaneous/sequel.rb
+++ lib/spontaneous/sequel.rb
@@ -2,6 +2,17 @@ require "sequel"
 
 Sequel.extension :inflector
 
+# See http://sequel.jeremyevans.net/rdoc/classes/Sequel/Timezones.html
+# UTC is more performant than :local (or 'nil' which just fallsback to :local)
+# A basic profiling run gives a 2 x performance improvement of :utc over :local
+# With ~240 rows, timing ::Content.all gives:
+#
+#     :utc     ~0.04s
+#     :local   ~0.08s
+#
+# DB timestamps are only shown in the editing UI & could be localized there per-user
+Sequel.default_timezone = :utc
+
 require 'sequel/plugins/serialization'
 
 Sequel::Plugins::Serialization.register_format(
