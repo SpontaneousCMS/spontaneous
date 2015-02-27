5c10d0076b2642e78b765009b4459f2e8a88b69e
Garry Hill <garry@magnetised.net>
2015-02-27 09:23:38 +0000

    Remove un-used serialization config

--- lib/spontaneous/sequel.rb
+++ lib/spontaneous/sequel.rb
@@ -12,17 +12,3 @@ Sequel.extension :inflector
 #
 # DB timestamps are only shown in the editing UI & could be localized there per-user
 Sequel.default_timezone = :utc
-
-require 'sequel/plugins/serialization'
-
-Sequel::Plugins::Serialization.register_format(
-  :ojson,
-  lambda { |v| Yajl::Encoder.new.encode(v) },
-  lambda { |v| Yajl::Parser.new(:symbolize_keys => true).parse(v)   }
-)
-# Sequel::Plugins::Serialization.register_format(
-#   :ojson,
-#   lambda { |v| Oj.dump(v) },
-#   lambda { |v| Oj.load(v, symbol_keys: true)   }
-# )
-
