aaff47d10e0d5ef284fc06f1510f109b2dd8b36d
Garry Hill <garry@magnetised.net>
2015-06-19 11:04:02 +0100

    This migration must run after the box content transition

--- db/migrations/20150320130000_fix_hidden_origins.rb
+++ db/migrations/20150320130000_fix_hidden_origins.rb
@@ -9,17 +9,18 @@ Sequel.migration do
     # Don't run unless we're in a full Spontaneous instance i.e. not in a
     # testing environment
     if defined?(Content)
-      aliases = Content.exclude(target: nil)
+      aliases = Content.exclude(target_id: nil)
       aliases.each do |a|
         target = a.target
         if target.hidden?
-          a.set_visible!(false, target.id)
+          a.send :apply_set_visible, false, target.id
+          a.save
         else
           # The bug set the hidden origin to the id of the parent, not the target
           # so we want to clear that unless the alias was actually hidden directly
           # i.e. has a hidden_origin == nil
           unless a.hidden_origin.nil?
-            a.apply_set_visible(true, nil)
+            a.send :apply_set_visible, true, nil
             a.save
           end
         end
