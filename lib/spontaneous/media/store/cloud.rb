2df256473ad7bc56578b73b1be5e4bc516ea63c8
Garry Hill <garry@magnetised.net>
2016-06-20 14:30:41 +0100

    Lose note-to-self

--- lib/spontaneous/media/store/cloud.rb
+++ lib/spontaneous/media/store/cloud.rb
@@ -8,34 +8,7 @@ module Spontaneous::Media::Store
     # Thanks thoughtbot (via paperclip)
     # http://rdoc.info/github/thoughtbot/paperclip/Paperclip/Storage/Fog
     AWS_BUCKET_SUBDOMAIN_RESTRICTON_REGEX = /^(?:[a-z]|\d(?!\d{0,2}(?:\.\d{1,3}){3}$))(?:[a-z0-9]|\.(?![\.\-])|\-(?![\.])){1,61}[a-z0-9]$/
-    #  bucket_name = "media.kitmonsters.com"
-    #  connection = Fog::Storage.new({
-    #     :provider=>"AWS",
-    #     :aws_secret_access_key=>"SECRET_ACCESS_KEY",
-    #     :aws_access_key_id=>"ACCESS_KEY_ID"
-    #  })
-    # connection.put_object(bucket_name, "test.jpg", File.open("/Users/garry/Dropbox/Photos/360343585_acc8e25bfa_o.jpg"), {
-    #   'x-amz-acl'=>"public-read",
-    #   "Cache-Control"=> "max-age=315360000, public",
-    #   "Expires"=>"Thu, 31 Dec 2037 23:55:55 GMT"
-    # })
-    # ===========================================================
-    # bucket_name = 'uploadtest.magnetised.net'
-    # bucket = storage.directories.get(bucket_name)
-    # p bucket
-    #
-    # large = nil
-    #
-    # bm = Benchmark.measure do
-    #   File.open('largefile.png', 'r') do |file|
-    #     large = bucket.files.create(:key => 'largefile.png', :body => file, :idempotent => false, :public => true)
-    #     # storage.put_object(bucket_name, 'largefile.png', file)
-    #   end
-    # end
-    # puts "Took #{bm.real} seconds"
-    # p storage.get_object_acl(bucket.key, large.key).body['AccessControlList']
-    # p large.public_url
-    #
+
     attr_reader :bucket_name
 
     # Don't verify my ssl certs when uploading images
